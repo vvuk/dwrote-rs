16d60d79ca05b2e0700dd9873ac32af7e4776a7d
MSleepyPanda <m.sleepypanda@gmail.com>
2016-12-10 13:42:30 +0100

    Extend GdiInterop implementation

--- src/gdi_interop.rs
+++ src/gdi_interop.rs
@@ -7,7 +7,7 @@ use std::cell::UnsafeCell;
 
 use comptr::ComPtr;
 use winapi;
-use super::{DWriteFactory, BitmapRenderTarget};
+use super::{BitmapRenderTarget, DWriteFactory, Font, FontFace};
 
 #[derive(Debug)]
 pub struct GdiInterop {
@@ -27,6 +27,47 @@ impl GdiInterop {
     pub fn take(native: ComPtr<winapi::IDWriteGdiInterop>) -> GdiInterop {
         GdiInterop {
             native: UnsafeCell::new(native),
+        }
+    }
+
+    pub fn create_font_from_logfont(&self, logfont: &winapi::LOGFONTW) -> Font {
+        unsafe {
+            let mut native: ComPtr<winapi::IDWriteFont> = ComPtr::new();
+            let logfont = logfont as *const _;
+            let hr = (*self.native.get()).CreateFontFromLOGFONT(logfont, native.getter_addrefs());
+            assert!(hr == 0);
+            Font::take(native)
+        }
+    }
+
+    pub fn convert_font_to_logfont(&self, font: Font) -> winapi::LOGFONTW {
+        unsafe {
+            let pointer: *mut winapi::LOGFONTW = ptr::null_mut();
+            let is_system_font: *mut winapi::BOOL = ptr::null_mut();
+            let hr = (*self.native.get()).ConvertFontToLOGFONT(font.as_ptr(),
+                                                               pointer,
+                                                               is_system_font);
+            assert!(hr == 0);
+            *pointer
+
+        }
+    }
+
+    pub fn convert_font_face_to_logfont(&self, face: FontFace) -> winapi::LOGFONTW {
+        unsafe {
+            let pointer: *mut winapi::LOGFONTW = ptr::null_mut();
+            let hr = (*self.native.get()).ConvertFontFaceToLOGFONT(face.as_ptr(), pointer);
+            assert!(hr == 0);
+            *pointer
+        }
+    }
+
+    pub fn create_font_face_from_hdc(&self, hdc: winapi::HDC) -> FontFace {
+        unsafe {
+            let mut native: ComPtr<winapi::IDWriteFontFace> = ComPtr::new();
+            let hr = (*self.native.get()).CreateFontFaceFromHdc(hdc, native.getter_addrefs());
+            assert!(hr == 0);
+            FontFace::take(native)
         }
     }
 
